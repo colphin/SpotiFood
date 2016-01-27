fcb401acadac2596865be59b2ef8bd6ae7a00d03
Matthew Cheung <matthewcheung@umail.ucsb.edu>
2016-01-26 21:30:25 -0800

    MC/TL Strong Added a nav bar and worked on routing pages

---
+++ Spotifood/client/templates/application/helpers.js
@@ -0,0 +1,17 @@
+if (Meteor.isClient) {
+  Template.navItem1.helpers({
+    activeIfTemplateIs: function (template) {
+      var currentRoute = Router.current();
+      return currentRoute &&
+        template === currentRoute.lookupTemplate() ? 'active' : '';
+    }
+  });
+  
+  Template.navItem2.helpers({
+    activeIfTemplateIs: function (template) {
+      var currentRoute = Router.current();
+      return currentRoute &&
+        template === currentRoute.lookupTemplate() ? 'active' : '';
+    }
+  });
+}
