c4fae47e69746ac38b7846e4793efd1e00eddfa5
Calvin Yin <calvin_yin@umail.ucsb.edu>
2016-01-26 20:25:38 -0800

    Finished Login and Logout

--- Spotifood/client/templates/application/login.js
+++ Spotifood/client/templates/application/login.js
@@ -1,8 +1,8 @@
 Template.login.events({
 	"submit form": function(event, template){
 		event.preventDefault();
-		var emailvar = template.find("#email").value;
-		var passwordvar = template.find('#password').value;
-		console.log('form submitted');
+		var emailvar = template.find("#login-email").value;
+		var passwordvar = template.find('#login-password').value;
+		Meteor.loginWithPassword(emailvar,passwordvar);
 	}
 })
