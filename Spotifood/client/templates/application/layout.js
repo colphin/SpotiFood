e51a153949082844a1446d22089f3c7d82099406
Calvin Yin <calvin_yin@umail.ucsb.edu>
2016-02-11 21:21:58 -0800

    idk what is broken and i'm too tired to care

--- Spotifood/client/templates/application/layout.js
+++ Spotifood/client/templates/application/layout.js
@@ -1,4 +1,4 @@
-Template.home.events({
+Template.layout.events({
 	'click.logout':function(event){
 		event.preventDefault();
 		Meteor.logout();
--- Spotifood/client/templates/application/register.js
+++ Spotifood/client/templates/application/register.js
@@ -1,8 +1,8 @@
 Template.register.events({
 	"submit form": function(event, template){
 		event.preventDefault();
-		var emailVar = template.find("#email").value;
-		var passwordVar = template.find('#password').value;
+		var emailVar = template.find("#inputEmail").value;
+		var passwordVar = template.find('#inputPassword').value;
 		// console.log('form submitted');
 		Accounts.createUser({
 			email:emailVar,
